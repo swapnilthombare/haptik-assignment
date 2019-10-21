9fedb05da4d5292756ea4a2100db27cca5307985
Swapnil Thombare <swapnilthombare812@gmail.com>
2019-10-21 15:05:03 +0530

    Updates

---
+++ precache-manifest.0b37d8e1a044730ba8a644279864fcc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee600b5c61b24701a2e1cf9c124380f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48cb803b5f7a840d16a9",
+    "url": "/static/css/main.a8868dda.chunk.css"
+  },
+  {
+    "revision": "579b6d96e032da47adef",
+    "url": "/static/js/2.beca9b25.chunk.js"
+  },
+  {
+    "revision": "48cb803b5f7a840d16a9",
+    "url": "/static/js/main.4eeaf402.chunk.js"
+  },
+  {
+    "revision": "8b7ccaca11d62c9e1a69",
+    "url": "/static/js/runtime-main.bd4aa45f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
