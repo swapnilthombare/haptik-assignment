ac73eb121cbdfd1c8cc78d1518a3c233ba2f7817
Swapnil Thombare <swapnilthombare812@gmail.com>
2019-10-22 00:35:14 +0530

    Updates

---
+++ precache-manifest.7c02099c8bbb110167c97782e8b8c53f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b78055adac52924f16d572edea094a5f",
+    "url": "/haptik-assignment/index.html"
+  },
+  {
+    "revision": "3b144d3653fb15742f35",
+    "url": "/haptik-assignment/static/css/main.a8868dda.chunk.css"
+  },
+  {
+    "revision": "30d5a417db7287f09aef",
+    "url": "/haptik-assignment/static/js/2.4eef5018.chunk.js"
+  },
+  {
+    "revision": "3b144d3653fb15742f35",
+    "url": "/haptik-assignment/static/js/main.b7d06065.chunk.js"
+  },
+  {
+    "revision": "fb065523dcb5fd235f50",
+    "url": "/haptik-assignment/static/js/runtime-main.3ad37275.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/haptik-assignment/static/media/logo.25bf045c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0b37d8e1a044730ba8a644279864fcc2.js"
+  "/haptik-assignment/precache-manifest.7c02099c8bbb110167c97782e8b8c53f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/haptik-assignment/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
